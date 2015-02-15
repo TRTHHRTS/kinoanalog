a4bb4af5a32b9ff0324b61196fc91f5461444dd5
Silakov Igor <kaluganin514@gmail.com>
2015-02-15 09:18:47 +0300

    properly project

---
+++ kino/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kino::Application.config.secret_key_base = '1d7c1b67d67be6f0aa75f72bbfa4fcc9a873cf1e06cac0266209cd2c83ba7c29052f8619fac6fa3ff62bb75e02c7cec7252af4400673ea79122ecfad5412ce8c'
---
+++ kino/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kino::Application.config.session_store :cookie_store, key: '_kino_session'
