5e0bc279ba5fff0390989a436c6e130b02db8e72
Исаев <abbas.isaev@gmail.com>
2015-04-16 15:29:25 +0300

    Имя при регистрации
    Поле имя пользоватлея при регистрации теперь обязательное при вводе

--- kino/db/migrate/20150314170025_devise_create_users.rb
+++ kino/db/migrate/20150314170025_devise_create_users.rb
@@ -4,12 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :encrypted_password, null: false
       t.string :email,              null: false
+      t.string :name,               null: false
     end
 
     add_index :users, :email, unique: true
 
-    # TODO если убрать default: '' см ниже, то rake:migrate выдает ошибку на эту строчку
-    add_column :users, :name, :string, null: false, default: '%username%'
     add_column :users, :sex, :string, null: false, default: 'Неизвестно'
     add_column :users, :permission, :integer, null:false, default: 3
   end
