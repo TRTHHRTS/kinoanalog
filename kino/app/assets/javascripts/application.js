0af5813fb0d4778cf1386837314e679494d203c6
Silakov Igor <kaluganin514@gmail.com>
2015-04-24 00:37:33 +0300

    Добавление фильма. Редактирование фильма (наконец). Такое ощущение, что что-то сломал -_- Но вроде все работает.
    P.S. Нахардкодил - в другой раз обязательно отрефакторю.

--- kino/app/assets/javascripts/application.js
+++ kino/app/assets/javascripts/application.js
@@ -13,6 +13,102 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+
+$(document).ready(function(){
+    var list = $("ul#selected_stars");
+    var inputs = list.find('input');
+    $.each(inputs, function(index, value){
+        var starName = value.name.slice(6, -1);
+        var starUrl = $(value).val();
+        var starId = value.id;
+        if (starUrl == '-1') {
+            list.append($('<li class="selected-item" id="' + starId + '">' + starName +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'stars\',' + starId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        } else {
+            list.append($('<li class="selected-item" id="' + starId + '"><a target="_blank" href="' + starUrl + '">'+ starName +'</a>' +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'stars\',' + starId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        }
+    });
+
+    list = $("ul#selected_directors");
+    inputs = list.find('input');
+    $.each(inputs, function(index, value){
+        var dirName = value.name.slice(10, -1);
+        var dirUrl = $(value).val();
+        var dirId = value.id;
+        if (dirUrl == '-1') {
+            list.append($('<li class="selected-item" id="' + dirId + '">' + dirName +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'directors\',' + dirId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        } else {
+            list.append($('<li class="selected-item" id="' + dirId + '"><a target="_blank" href="' + dirUrl + '">'+ dirName +'</a>' +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'directors\',' + dirId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        }
+    });
+
+    list = $("ul#selected_producers");
+    inputs = list.find('input');
+    $.each(inputs, function(index, value){
+        var prodName = value.name.slice(10, -1);
+        var prodUrl = $(value).val();
+        var prodId = value.id;
+        if (prodUrl == '-1') {
+            list.append($('<li class="selected-item" id="' + prodId + '">' + prodName +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'producers\',' + prodId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        } else {
+            list.append($('<li class="selected-item" id="' + prodId + '"><a target="_blank" href="' + prodUrl + '">'+ prodName +'</a>' +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'producers\',' + prodId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        }
+    });
+
+    list = $("ul#selected_writers");
+    inputs = list.find('input');
+    $.each(inputs, function(index, value){
+        var writerName = value.name.slice(8, -1);
+        var writerUrl = $(value).val();
+        var writerId = value.id;
+        if (writerUrl == '-1') {
+            list.append($('<li class="selected-item" id="' + writerId + '">' + writerName +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'writers\',' + writerId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        } else {
+            list.append($('<li class="selected-item" id="' + writerId + '"><a target="_blank" href="' + writerUrl + '">'+ writerName +'</a>' +
+                '<a class="btn btn-xs" href="javascript:deleteItem(\'writers\',' + writerId + ')">' +
+                '<img src="/assets/remove-orange.png"/></a></li>'));
+        }
+    });
+
+    var items = 'countries';
+    list = $("ul#selected_countries");
+    inputs = list.find('input');
+    $.each(inputs, function(index, value){
+        var cName = $(value).val();
+        var cId = value.id;
+        list.append($('<li class="selected-item" id="' + cId + '">' + cName +
+            '<a class="btn btn-xs" href="javascript:deleteItem(\'' + items + '\',' + cId + ')">' +
+            '<img src="/assets/remove-orange.png"/></a></li>'));
+        $('select#country_select option:contains('+ cName + ')').remove();
+    });
+
+    items = 'genres';
+    list = $("ul#selected_genres");
+    inputs = list.find('input');
+    $.each(inputs, function(index, value){
+        var cName = $(value).val();
+        var cId = value.id;
+        list.append($('<li class="selected-item" id="' + cId + '">' + cName +
+            '<a class="btn btn-xs" href="javascript:deleteItem(\'' + items + '\',' + cId + ')">' +
+            '<img src="/assets/remove-orange.png"/></a></li>'));
+        $('select#genre_select option:contains('+ cName + ')').remove();
+    });
+})
+
+
 function addItem(itemName) {
     var items;
     if (itemName == 'country') {
@@ -65,7 +161,7 @@ function addItemWithUrl(itemName) {
 
 function deleteItem(c, id) {
     var removingItem = $("ul#selected_"+c);
-    var itemName = removingItem.find("#" + id);
+    var itemName = removingItem.find("li#" + id);
     removingItem.children().remove("#" + id);
     if (c == 'countries') {
         c = 'country'
--- kino/app/controllers/movies_controller.rb
+++ kino/app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: [:edit_movie]
+  before_action :fill_movie_by_params, only: [:create_movie, :update_movie]
   before_action :movie_params, only: [:create_movie, :update_movie]
   before_action :check_only_admin_moder, only: [:new_movie, :create_movie, :edit_movie, :update_movie, :destroy_movie]
 
@@ -10,94 +10,19 @@ class MoviesController < ApplicationController
 
   # GET
   def edit_movie
+    @movie = Movie.find(params[:id])
+    params[:title] = @movie.title
+    params[:orig_title] = @movie.orig_title
+    params[:release_date] = @movie.release_date
+    params[:duration] = @movie.duration
+    params[:age_id] = @movie.age_id
+    params[:description] = @movie.description
   end
 
   # POST
   def create_movie
-    @movie = Movie.new
     @movie.id = Movie.last.id + 1
-    @movie.title = params[:title]
-    @movie.orig_title = params[:orig_title]
-    @movie.release_date = params[:release_date]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.age_id = params[:age_id]
-    # Добавление стран к сущности фильма
-    unless params[:countries].nil?
-      params[:countries].each { |c|
-        @movie.countries << Country.find_by_name(c)
-      }
-    end
-    # Добавление жанров к сущности фильма
-    unless params[:genres].nil?
-      params[:genres].each { |g|
-        @movie.genres << Genre.find_by_name(g)
-      }
-    end
-    # Добавление актеров к сущности фильма и их сохранение
-    unless params[:stars].nil?
-      params[:stars].each { |k,v|
-        star = Star.find_by_name(k)
-        if star.nil?
-          star = Star.new(id:Star.last.id + 1, name: k, url: v)
-          star.save
-        else
-          if star.url.nil?
-            star.url=v
-            star.save
-          end
-        end
-        @movie.stars << star
-      }
-    end
-    # Добавление режиссеров к сущности фильма и их сохранение
-    unless params[:directors].nil?
-      params[:directors].each { |k,v|
-        director = Director.find_by_name(k)
-        if director.nil?
-          director = Director.new(id:Director.last.id + 1, name: k, url: v)
-          director.save
-        else
-          if director.url.nil?
-            director.url=v
-            director.save
-          end
-        end
-        @movie.directors << director
-      }
-    end
-    # Добавление продюсеров к сущности фильма и их сохранение
-    unless params[:producers].nil?
-      params[:producers].each { |k,v|
-        producer = Producer.find_by_name(k)
-        if producer.nil?
-          producer = Producer.new(id:Producer.last.id + 1, name: k, url: v)
-          producer.save
-        else
-          if producer.url.nil?
-            producer.url=v
-            producer.save
-          end
-        end
-        @movie.producers << producer
-      }
-    end
-    # Добавление сценаристов к сущности фильма и их сохранение
-    unless params[:writers].nil?
-      params[:writers].each { |k,v|
-        writer = Writer.find_by_name(k)
-        if writer.nil?
-          writer = Writer.new(id:Writer.last.id + 1, name: k, url: v)
-          writer.save
-        else
-          if writer.url.nil?
-            writer.url=v
-            writer.save
-          end
-        end
-        @movie.writers << writer
-      }
-    end
+    @movie.age_id = params[:movie][:age_id]
     # Сохранение постера
     poster = params[:file][:poster] unless params[:file].nil?
     if poster.nil?
@@ -121,27 +46,31 @@ class MoviesController < ApplicationController
     end
   end
 
-
   # PATCH/PUT
   def update_movie
-    #TODO Переделать по аналогии с добавлением фильма.
-    @movie = Movie.find(params[:id])
-
-    uploadedFile = params[:movie][:poster]
-    unless uploadedFile.nil?
+    @movieFromDb = Movie.find(params[:id])
+    @movie.image_url = @movieFromDb.image_url
+    @movie.id = @movieFromDb.id
+    @movie.age_id = params[:movie][:age_id]
+    # Сохранение постера
+    poster = params[:file][:poster] unless params[:file].nil?
+    unless poster.nil?
       unless @movie.image_url.split('/').include? 'noposter.png'
         File.delete('public' + @movie.image_url)
       end
-      extension = uploadedFile.original_filename.split('.').last
+      extension = poster.original_filename.split('.').last
       directory = '/assets/images/movies'
       path = File.join(directory, @movie.id.to_s + '.' + extension)
-      File.open('public'+path, 'wb') { |f| f.write(uploadedFile.read) }
-      params[:movie][:image_url] = path
+      File.open('public'+path, 'wb') { |f| f.write(poster.read) }
+      @movie.image_url = path
+      params[:file].delete(:poster)
     end
-    params[:movie].delete(:poster)
     respond_to do |format|
-      if @movie.update(movie_params)
-        format.html { redirect_to '/details/'+@movie.id.to_s, notice: 'Информация о фильме успешно изменена' }
+      if @movieFromDb.update(@movie.attributes)
+        if @movieFromDb.update_attributes(countries: @movie.countries, genres: @movie.genres,
+                                          stars: @movie.stars, directors: @movie.directors, producers: @movie.producers, writers: @movie.writers)
+          format.html { redirect_to '/details/'+@movie.id.to_s, notice: 'Информация о фильме успешно изменена' }
+      end
       else
         format.html { render action: 'edit_movie' }
       end
@@ -168,8 +97,98 @@ class MoviesController < ApplicationController
   end
 
   private
-    def set_movie
-      @movie = Movie.find(params[:id])
+
+  # Заполнить сущность Movie параметрами из добавления или из редактирования
+    def fill_movie_by_params
+      @movie = Movie.new
+
+      @movie.title = params[:title]
+      @movie.orig_title = params[:orig_title]
+      @movie.release_date = params[:release_date]
+      @movie.duration = params[:duration]
+      @movie.description = params[:description]
+      @movie.age_id = params[:age_id]
+      # Добавление стран к сущности фильма
+      unless params[:countries].nil?
+        params[:countries].each { |c|
+          @movie.countries << Country.find_by_name(c)
+        }
+      end
+      # Добавление жанров к сущности фильма
+      unless params[:genres].nil?
+        params[:genres].each { |g|
+          @movie.genres << Genre.find_by_name(g)
+        }
+      end
+      # Добавление актеров к сущности фильма и их сохранение
+      unless params[:stars].nil?
+        params[:stars].each { |k,v|
+          star = Star.find_by_name(k)
+          if star.nil?
+            if v == '-1'
+              star = Star.new(id:Star.last.id + 1, name: k, url: nil)
+            else
+              star = Star.new(id:Star.last.id + 1, name: k, url: v)
+            end
+
+            star.save
+          else
+            if star.url.nil? && v != '-1'
+              star.url=v
+              star.save
+            end
+          end
+          @movie.stars << star
+        }
+      end
+      # Добавление режиссеров к сущности фильма и их сохранение
+      unless params[:directors].nil?
+        params[:directors].each { |k,v|
+          director = Director.find_by_name(k)
+          if director.nil?
+            director = Director.new(id:Director.last.id + 1, name: k, url: v)
+            director.save
+          else
+            if director.url.nil?
+              director.url=v
+              director.save
+            end
+          end
+          @movie.directors << director
+        }
+      end
+      # Добавление продюсеров к сущности фильма и их сохранение
+      unless params[:producers].nil?
+        params[:producers].each { |k,v|
+          producer = Producer.find_by_name(k)
+          if producer.nil?
+            producer = Producer.new(id:Producer.last.id + 1, name: k, url: v)
+            producer.save
+          else
+            if producer.url.nil?
+              producer.url=v
+              producer.save
+            end
+          end
+          @movie.producers << producer
+        }
+      end
+      # Добавление сценаристов к сущности фильма и их сохранение
+      unless params[:writers].nil?
+        params[:writers].each { |k,v|
+          writer = Writer.find_by_name(k)
+          if writer.nil?
+            writer = Writer.new(id:Writer.last.id + 1, name: k, url: v)
+            writer.save
+          else
+            if writer.url.nil?
+              writer.url=v
+              writer.save
+            end
+          end
+          @movie.writers << writer
+        }
+      end
     end
 
   def movie_params
