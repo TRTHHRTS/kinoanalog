43e6c80df2968702af3fd6e3c1ff2b4551ca89c8
Silakov Igor <kaluganin514@gmail.com>
2015-05-10 02:46:30 +0300

    Дизайн, баги по дизайну. Настройка для редиректа после логина.

--- kino/app/controllers/application_controller.rb
+++ kino/app/controllers/application_controller.rb
@@ -5,6 +5,26 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  after_filter :store_location
+
+  def store_location
+    # сохраянем последний URL - нужно, чтобы при логине или регистрации редиректило на предыдущую страницу.
+    return unless request.get?
+    if request.path != '/users/sign_in' &&
+        request.path != '/users/sign_up' &&
+        request.path != '/users/password/new' &&
+        request.path != '/users/password/edit' &&
+        request.path != '/users/confirmation' &&
+        request.path != '/users/sign_out' &&
+        !request.xhr? # не храним вызовы ajax
+      session[:previous_url] = request.fullpath
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
   protected
 
   def configure_permitted_parameters
