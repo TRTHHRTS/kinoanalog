5471d3ed600dbc0ccce92f83931d545f2ae309b0
Исаев <abbas.isaev@gmail.com>
2015-05-14 14:11:38 +0300

    Отчет 6 для печати
    Последний отчет в ожидании печати

--- kino/app/assets/javascripts/extended_search.js
+++ kino/app/assets/javascripts/extended_search.js
@@ -1,10 +1,13 @@
+var oldValueObj = null;
+
 function findList(obj) {
     var nameObj=obj.name;
     var valueObj=obj.value;
     var jsListId=obj.list.id;
 
-    if (valueObj.length >= 3)
+    if (valueObj.length >= 3 && oldValueObj != valueObj)
     {
+        oldValueObj = valueObj;
         $("#"+jsListId).empty();
         var dataList = document.getElementById(jsListId);
         $.ajax({
